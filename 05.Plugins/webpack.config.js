b76de8bf60e2b63922987c32ebcb28f9be35d8d7
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-07-22 08:54:45 -0600

    Changes for the class '26. Generating HTML files automatically during webpack build process'

--- 05.Plugins/webpack.config.js
+++ 05.Plugins/webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: 'dist/',
+        publicPath: '',
         // clean: {
         //     dry: true,
         //     keep: /\.css/
@@ -67,5 +68,6 @@ module.exports = {
             //     ]
             // }
         ),
+        new HtmlWebpackPlugin()
     ]
 };
