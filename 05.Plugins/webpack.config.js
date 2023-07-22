06ebcc8d6d24768111f62c84713103965aafdf28
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-07-22 08:46:43 -0600

    Changes for the class '25. How to clean dist folder before generating new bundles'

--- 05.Plugins/webpack.config.js
+++ 05.Plugins/webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: 'dist/'
+        publicPath: 'dist/',
+        // clean: {
+        //     dry: true,
+        //     keep: /\.css/
+        // }
     },
     mode: 'none',
     module: {
@@ -53,6 +58,14 @@ module.exports = {
         new TerserPlugin(),
         new MiniCssExtractPlugin({
             filename: 'styles.[contenthash].css',
-        })
+        }),
+        new CleanWebpackPlugin(
+            //     {
+            //     cleanOnceBeforeBuildPatterns: [
+            //         '**/*',
+            //         path.join(process.cwd(), 'build/**/*')
+            //     ]
+            // }
+        ),
     ]
 };
