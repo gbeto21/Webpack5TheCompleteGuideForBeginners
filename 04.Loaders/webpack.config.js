455a9ce0611de48b9e8bc0a30c0c23686862e123
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-07-21 20:39:59 -0600

    Changes for the class '16. Handling CSS with webpack'

--- 04.Loaders/webpack.config.js
+++ 04.Loaders/webpack.config.js
@@ -1,28 +1,34 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/'
-  },
-  mode: 'none',
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg)$/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 3 * 1024 //# kilobytes
-          }
-        }
-      },
-      {
-        test: /\.txt/,
-        type: 'asset/source'
-      }
-    ]
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 3 * 1024
+                    }
+                }
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            }
+        ]
+    }
 };
